28a6702cb4055ede20b08021e4a400a42617700f
Anna Goldenberg <Anna@dyn-209-2-214-200.dyn.columbia.edu>
2013-03-25 23:36:19 -0400

    updated bio, added articles, updated captions of carousel

--- server/client/js/data.js
+++ server/client/js/data.js
@@ -44,9 +44,77 @@ var print = {
 				url: 'http://www.varsity.co.uk/lifestyle/4047',
 				date: '2011/11/14'
 			}, {
+				title: 'Second-rate salary. Getting a 2.i will make you rich, but getting a First won\'t make you much richer',
+				url: 'varsity_743.pdf',
+				date: '2011/10/14'
+			}, {	
 				title: 'The Genes Behind Your Driving Licence',
 				url: 'http://www.varsity.co.uk/features/2480',
-				date: '2010/10/08'
+				date: '2010/10/08'	
+			}]
+		}, {
+			title: 'The Cambridge Student',
+			location: 'Cambridge, United Kingdom',
+			articles: [{
+				title: 'Eyewitness: Austrian Students in Protest',
+				url: 'http://www.tcs.cam.ac.uk/issue/international/eyewitness-austrian-students-in-protest/#more-4825',
+				date: '2009/11/21'
+			},{	
+				title: 'Hour Flower Power. How a children\'s book and an American psychologist show us the importance of time',
+				url: './pdf/TCS_HourFlowerPower.pdf',
+				date: '2010/04/22'
+			}]
+		}, {
+			title: 'profil wissen',
+			location: 'Vienna, Austria',
+			articles: [{
+				title: 'Einblick: Erschütternd (Erdbebenkarte)',
+				url: './pdf/prof1203_wiss_EINBLICK.pdf',
+				date: '2012/09/05'
+			}, {
+				title: 'Zahltag',
+				url: './pdf/prof1203_wiss_ZAHLTAG.pdf',
+				date: '2012/09/05'
+			}, {
+				title: 'Einblick: Sterndeutung',
+				url: './pdf/prof1202_wissen_EINBLICK.pdf',
+				date: '2012/06/11'		
+			}, {
+				title: 'Zahltag',
+				url: './pdf/prof1202_wissen_ZAHLTAG.pdf',
+				date: '2012/06/11'	
+			}, {
+				title: 'Einblick: Codename Enigma (Alan Turing)',
+				url: './pdf/prof1201_wiss_EINBLICK-1.pdf',
+				date: '2012/03/26'	
+			}, {
+				title: 'Zahltag',
+				url: './pdf/prof1201_wiss_ZAHLTAG-1.pdf',
+				date: '2012/03/26'	
+			}, {
+				title: 'Einblick: Die Schatzkarte (Schiffwracks)',
+				url: './pdf/prof1101_wissen_EINBLICK.pdf',
+				date: '2011/10/31'	
+			}, {
+				title: 'Zahltag',
+				url: './pdf/prof1101_wissen_ZAHLTAG.pdf',
+				date: '2011/10/31'
+			}]
+		}, {
+			title: 'profil',
+			location: 'Vienna, Austria',
+			articles: [{
+				title: 'Systemausfall. World Wide Web: Macht das Internet dumm? (mit Tina Goebel)',
+				url: 'http://www.profil.at/articles/1209/560/320828/macht-internet-systemausfall',
+				date: '2012/03/02'	
+		}, {		
+				title: 'Der Ursprung des Menschen. Wie der Homo sapiens den Neanderthaler verdrängte (mit Tina Goebel)',
+				url: 'http://www.profil.at/articles/1131/560/303681/evolution-der-ursprung-menschen',
+				date: '2011/08/06'
+		}, {
+				title: 'Schockreaktion. Insektengiftallergien können ohne Vorwarnung auftreten. Eine Spritzenkur hilft, das Risiko zu verringern',
+				url: './pdf/prof1131_SCHOCK.pdf',
+				date: '2011/08/01'
 			}]
 		}
 	]
