696d945d5b563d027040ec01ecfb9acb329b09d2
Anna Goldenberg <Anna@Annas-MacBook-Air-2.local>
2013-03-26 20:41:09 -0400

    added German captions

--- server/client/js/data/visual.js
+++ server/client/js/data/visual.js
@@ -2,22 +2,29 @@ var visualData = {
     images: [
         {
             name: '1.jpg',
-            caption: 'A yellow cab on the Upper West Side in New York City'
+            caption: 'A yellow cab on the Upper West Side in New York City',
+            german: 'Ein typisches New Yorker Taxi an der Upper West Side in New York City'
+
         }, {
             name: '2.jpg',
-            caption: 'Battery Park in Manhattan on the day after superstorm Sandy hit New York on Oct. 29, 2012'
+            caption: 'Battery Park in Manhattan on the day after superstorm Sandy hit New York on Oct. 29, 2012',
+            german: 'Battery Park an Manhattans Südspitze am Tag nachdem Hurrikan Sandy auf die US-Ostküste traf (29. Oktober 2012)'
         }, {
             name: '3.jpg',
-            caption: 'Bird\'s-eye view of Lincoln Square, New York City'
+            caption: 'Bird\'s-eye view of Lincoln Square, New York City',
+            german: 'Lincoln Square in New York City aus der Vogelperspektive'
         }, {
             name: '4.jpg',
-            caption: 'La Mia, 26, left, and Ronald Lewis, 26, are best friends and walk La Mia\'s dog Chance at Central Park in New York City'
+            caption: 'La Mia, 26, left, and Ronald Lewis, 26, are best friends and walk La Mia\'s dog Chance at Central Park in New York City',
+            german: 'La Mia, 26, links und Ronald Lewis, 26, sind beste Freunde und gemeinsam mit La Mias Hund Chance im Central Park in New York City'
         }, {
             name: '5.jpg',
-            caption: 'Mark Mirasol, 24, and Lily Prentice, 23, at Central Park West in New York City'
+            caption: 'Mark Mirasol, 24, and Lily Prentice, 23, at Central Park West in New York City',
+            german: 'Mark Mirasol, 24, und Lily Prentice, 23, nahe des Central Parks in New York City'
         }, {
             name: '6.jpg',
-            caption: 'Houses on the Lower East Side, New York\'s historic immigrant district, reflected in windows of the Tenement Museum'
+            caption: 'Houses on the Lower East Side, New York\'s historic immigrant district, reflected in windows of the Tenement Museum',
+            german: 'In den Schaufenstern des Tenement Museums spiegeln sich die Häuser der Lower East Side, New Yorks ehemaligen Zuwandererviertels'
         }
     ]
 };
